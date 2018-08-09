9b19f71eb5fc464495ff30879908984585485534
Raimundo <junnyhormarks@gmail.com>
2018-08-08 20:30:48 -0400

    criacao do arquivo principal

---
+++ calcular-area.c
@@ -0,0 +1,17 @@
+/*
+	Oficina GIT
+	Professor: Orlewilson B. Maia
+	Equipe: 02
+	Data: 08/08/2018
+	Descrição: Calcular area de
+		   circulo, triangulo
+		   equilatero e quadrado		
+*/
+
+#include<stdio.h>
+
+int main(){
+
+	printf("Hello World\n");
+	return 0;
+}
